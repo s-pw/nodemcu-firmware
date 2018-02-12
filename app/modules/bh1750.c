d0a42e2e082cf018a3250ebb7af770553d784675
Pawel Switalski <switalski.pw@gmail.com>
2018-02-11 18:15:30 -0800

    Add support for BH1750FVI ambient light sensors

--- app/modules/bh1750.c
+++ app/modules/bh1750.c
@@ -6,47 +6,139 @@
 #include "lauxlib.h"
 #include "platform.h"
 
-#define BH1750_ADDRESS           0x23
-/* BH1750 Commands */
-#define BH1750_CONTINUOUS_HIGH_RES_MODE	0x10
-#define RES_DIV                 (78642)
+#define BH1750_ADDRESS 0x23
 
-static int ICACHE_FLASH_ATTR bh1750_init(lua_State* L) {
-    uint32_t sda;
-    uint32_t scl;
+#define PLATFORM_BH1750_DEFAULT_SENSITIVITY  69
+#define PLATFORM_BH1750_POWER_DOWN  0x00
+#define PLATFORM_BH1750_MIN_SENSITIVITY  31
+#define PLATFORM_BH1750_MAX_SENSITIVITY  254
 
-    sda = luaL_checkinteger(L, 1);
-    scl = luaL_checkinteger(L, 2);
+typedef enum {
+    PLATFORM_BH1750_CONTINUOUS_AUTO = 0,
+    PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE  = 0x10,
+    PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE_2 = 0x11,
+    PLATFORM_BH1750_CONTINUOUS_LOW_RES_MODE = 0x13,
+    PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE = 0x20,
+    PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE_2 = 0x21,
+    PLATFORM_BH1750_ONE_TIME_LOW_RES_MODE = 0x23,
+} platform_bh1750_mode_t;
 
-    luaL_argcheck(L, sda > 0 && scl > 0, 1, "no i2c for D0");
+uint16_t platform_bh1750_sensitivity = PLATFORM_BH1750_DEFAULT_SENSITIVITY;
+uint8_t platform_bh1750_mode = PLATFORM_BH1750_CONTINUOUS_AUTO;
 
-    platform_i2c_setup(0, sda, scl, PLATFORM_I2C_SPEED_SLOW);
-
-    platform_i2c_send_start(0);
-    platform_i2c_send_address(0, BH1750_ADDRESS, PLATFORM_I2C_DIRECTION_TRANSMITTER);
-    platform_i2c_send_byte(0, BH1750_CONTINUOUS_HIGH_RES_MODE);
-    platform_i2c_send_stop(0);
-
-    return 0;
+static void ICACHE_FLASH_ATTR send_command( uint8_t command )
+{
+  platform_i2c_send_start(0);
+  platform_i2c_send_address(0, BH1750_ADDRESS, PLATFORM_I2C_DIRECTION_TRANSMITTER);
+  platform_i2c_send_byte(0, command);
+  platform_i2c_send_stop(0);
 }
 
-static int ICACHE_FLASH_ATTR bh1750_read(lua_State* L) {
-    uint32_t rawValue;
-    platform_i2c_send_start(0);
-    platform_i2c_send_address(0, BH1750_ADDRESS, PLATFORM_I2C_DIRECTION_RECEIVER);
-    rawValue = (uint16_t) platform_i2c_recv_byte(0, 1) << 8;
-    rawValue |= platform_i2c_recv_byte(0, 0);
-    platform_i2c_send_stop(0);
+static void ICACHE_FLASH_ATTR set_sensitivity( uint8_t sensitivity )
+{
+  send_command((uint8_t) (0b01000 << 3) | (sensitivity >> 5));
+  send_command((uint8_t) (0b011 << 5 )  | (uint8_t) (sensitivity & 0b11111));
 
-    lua_pushinteger(L, rawValue * 1000 / 12);
-
-    return 1;
+  if (platform_bh1750_mode == PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE_2 ||
+      platform_bh1750_mode == PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE_2 ||
+      (platform_bh1750_mode == PLATFORM_BH1750_CONTINUOUS_AUTO && sensitivity == PLATFORM_BH1750_MAX_SENSITIVITY)) {
+    platform_bh1750_sensitivity = (uint16_t) (((uint16_t) sensitivity) * 2);
+  } else {
+    platform_bh1750_sensitivity = sensitivity;
+  }
 }
+
+static void ICACHE_FLASH_ATTR platform_bh1750_setup( platform_bh1750_mode_t mode, uint8_t sensitivity )
+{
+  platform_bh1750_mode = mode;
+
+  if (mode == PLATFORM_BH1750_CONTINUOUS_AUTO) {
+    set_sensitivity(PLATFORM_BH1750_MAX_SENSITIVITY);
+    send_command(PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE_2);
+  } else {
+    if (PLATFORM_BH1750_MIN_SENSITIVITY >= 31 && PLATFORM_BH1750_MAX_SENSITIVITY <= 254) {
+      set_sensitivity(sensitivity);
+    }
+    send_command(mode);
+  }
+}
+
+static void ICACHE_FLASH_ATTR platform_bh1750_power_down()
+{
+  send_command(PLATFORM_BH1750_POWER_DOWN);
+}
+
+static uint32_t ICACHE_FLASH_ATTR platform_bh1750_read()
+{
+  uint32_t value;
+  platform_i2c_send_start(0);
+  platform_i2c_send_address(0, BH1750_ADDRESS, PLATFORM_I2C_DIRECTION_RECEIVER);
+  value = (uint16_t) platform_i2c_recv_byte(0, 1) << 8;
+  value |= platform_i2c_recv_byte(0, 0);
+  platform_i2c_send_stop(0);
+
+  uint32_t lux_value = (uint32_t) (value * 57500 / platform_bh1750_sensitivity);
+
+  if (platform_bh1750_mode == PLATFORM_BH1750_CONTINUOUS_AUTO) {
+    if (lux_value < 7200000 && platform_bh1750_sensitivity == PLATFORM_BH1750_MIN_SENSITIVITY) {
+      set_sensitivity(PLATFORM_BH1750_MAX_SENSITIVITY);
+      send_command(PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE_2);
+    } else if (lux_value >= 7000000 && platform_bh1750_sensitivity == PLATFORM_BH1750_MAX_SENSITIVITY * 2) {
+      set_sensitivity(PLATFORM_BH1750_MIN_SENSITIVITY);
+      send_command(PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE);
+    }
+  }
+
+  return lux_value;
+}
+
+static int ICACHE_FLASH_ATTR bh1750_setup(lua_State *L)
+{
+  platform_bh1750_mode_t mode = (platform_bh1750_mode_t) luaL_optinteger( L, 1, PLATFORM_BH1750_CONTINUOUS_AUTO );
+  uint8_t sensitivity = (uint8_t) luaL_optinteger( L, 2, PLATFORM_BH1750_DEFAULT_SENSITIVITY );
+
+  luaL_argcheck( L,
+                 mode == PLATFORM_BH1750_CONTINUOUS_AUTO ||
+                 mode == PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE ||
+                 mode == PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE_2 ||
+                 mode == PLATFORM_BH1750_CONTINUOUS_LOW_RES_MODE ||
+                 mode == PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE ||
+                 mode == PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE_2 ||
+                 mode == PLATFORM_BH1750_ONE_TIME_LOW_RES_MODE, 1, "wrong mode" );
+
+  luaL_argcheck( L, sensitivity >= PLATFORM_BH1750_MIN_SENSITIVITY && sensitivity <= PLATFORM_BH1750_MAX_SENSITIVITY, 2,
+                 "invalid sensitivity" );
+
+  platform_bh1750_setup(mode, sensitivity);
+
+  return 0;
+}
+
+static ICACHE_FLASH_ATTR int bh1750_power_down(lua_State *L)
+{
+  platform_bh1750_power_down();
+  return 0;
+}
+
+static ICACHE_FLASH_ATTR int bh1750_read(lua_State *L)
+{
+  lua_pushinteger(L, platform_bh1750_read());
+  return 1;
+}
+
 
 static const LUA_REG_TYPE bh1750_map[] = {
-    { LSTRKEY( "read" ),         LFUNCVAL( bh1750_read )},
-    { LSTRKEY( "init" ),         LFUNCVAL( bh1750_init )},
-    { LNILKEY, LNILVAL}
+    { LSTRKEY( "setup" ),                      LFUNCVAL( bh1750_setup ) },
+    { LSTRKEY( "power_down" ),                 LFUNCVAL( bh1750_power_down ) },
+    { LSTRKEY( "read" ),                       LFUNCVAL( bh1750_read ) },
+    { LSTRKEY( "CONTINUOUS_AUTO" ),            LNUMVAL( PLATFORM_BH1750_CONTINUOUS_AUTO ) },
+    { LSTRKEY( "CONTINUOUS_HIGH_RES_MODE" ),   LNUMVAL( PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE ) },
+    { LSTRKEY( "CONTINUOUS_HIGH_RES_MODE_2" ), LNUMVAL( PLATFORM_BH1750_CONTINUOUS_HIGH_RES_MODE_2 ) },
+    { LSTRKEY( "CONTINUOUS_LOW_RES_MODE" ),    LNUMVAL( PLATFORM_BH1750_CONTINUOUS_LOW_RES_MODE ) },
+    { LSTRKEY( "ONE_TIME_HIGH_RES_MODE" ),     LNUMVAL( PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE ) },
+    { LSTRKEY( "ONE_TIME_HIGH_RES_MODE_2" ),   LNUMVAL( PLATFORM_BH1750_ONE_TIME_HIGH_RES_MODE_2 ) },
+    { LSTRKEY( "ONE_TIME_LOW_RES_MODE" ),      LNUMVAL( PLATFORM_BH1750_ONE_TIME_LOW_RES_MODE ) },
+    {LNILKEY,         LNILVAL}
 };
 
 NODEMCU_MODULE(BH1750, "bh1750", bh1750_map, NULL);
